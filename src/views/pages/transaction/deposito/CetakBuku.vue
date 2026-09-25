5c0f2e3205351db3556bfb352ebd7633eaa35b94
ahapsin <121087803+ahapsin@users.noreply.github.com>
2026-09-25 13:48:41 +0700

    devin

--- src/views/pages/transaction/deposito/CetakBuku.vue
+++ src/views/pages/transaction/deposito/CetakBuku.vue
@@ -109,7 +109,7 @@
                                                 <n-text class="text-[12px]" strong>Branch Manager</n-text>
                                             </div>
                                             <div>
-                                                <n-text underline strong class="block">LYDIA MASEHI</n-text>
+                                                <n-text underline strong class="block">DEVIN CAHYANI</n-text>
                                                 <n-text class="text-[12px]" strong>Ketua</n-text>
                                             </div>
                                         </div>
